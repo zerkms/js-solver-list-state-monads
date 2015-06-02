17891f7158594cf79ce2a4d2819626afbf03c782
Ivan Kurnosov <zerkms@zerkms.com>
2015-06-02 22:27:20 +1200

    Some minor refactorings

--- utils.js
+++ utils.js
@@ -1,42 +1,37 @@
 'use strict';
 
-export const fmap = (f, lst) => lst.map(f);
-
 export const runStateList = (st, s) => st(s);
 export const evalStateList = (st, s) => st(s).map(p => p.first);
 
 export const _return = a => s => ({first: a, second: s});
 
+export const fmap = (f, lst) => lst.map(f);
 export const flatten = list => Array.prototype.concat.apply([], list);
+export const flatMap = (f, lst) => flatten(lst.map(f));
 
 export const nub = arr => arr.filter((v, i, self) => self.indexOf(v) === i);
 
 export const bind = (g, k) => {
     return s => {
         let plst = g(s);
-        let lst2 = fmap(p => {
+        return flatMap(p => {
             let { first: a, second: s1 } = p;
             let ka = k(a);
 
-            let result = runStateList(ka, s1);
-            return result;
+            return runStateList(ka, s1);
         }, plst);
-
-        return flatten(lst2);
     };
 };
 
 export const then = (g, k) => {
     return s => {
         let plst = g(s);
-        let lst2 = fmap(p => {
+        return flatMap(p => {
             let s1 = p.second;
             let ka = k();
-            let result = runStateList(ka, s1);
-            return result;
-        }, plst);
 
-        return flatten(lst2);
+            return runStateList(ka, s1);
+        }, plst);
     };
 };
 
